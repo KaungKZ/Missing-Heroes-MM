e5418371e473cc2e08f058dc3e3789989110b426
Kaung Khant Zaw <kaungkzdev@gmail.com>
2021-03-28 18:13:07 +0630

    added codes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fetch = require("node-fetch");
 
 const app = express();
 // const router = express.Router();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
